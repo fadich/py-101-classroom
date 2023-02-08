76200aa1b878bee5ddbf26de91102ba9dcf14029
Fadi Ahmad <fadich95@gmail.com>
2023-02-08 19:39:31 +0200

    2023-02-08

--- 2023-02-08/func_in_func_example.py
+++ 2023-02-08/func_in_func_example.py
@@ -1,17 +1,24 @@
 
-def circle_area(pi):
+def circle_area(pi, world_name: str):
+    count_calls = 0
+
     def count_area(r):
+        nonlocal count_calls
+
+        count_calls += 1
+        print(f"Called in {world_name} world {count_calls} times")
+
         return pi * (r ** 2)
 
     return count_area
 
 
-our_world = circle_area(3.14159)
+our_world = circle_area(3.14159, "OUR!")
 print(f"our {our_world(1)}")
 print(f"our {our_world(5)}")
 print(f"our {our_world(5.85)}")
 
-their_world = circle_area(4.14159)
+their_world = circle_area(4.14159, "THEIR")
 print(f"their {their_world(1)}")
 print(f"their {their_world(5)}")
 print(f"their {their_world(5.85)}")
