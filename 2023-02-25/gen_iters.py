c3180b4df24726940d50237b393115eb59ee3919
Fadi Ahmad <fadich95@gmail.com>
2023-02-27 20:10:47 +0200

    2023-02-27

--- 2023-02-25/gen_iters.py
+++ 2023-02-25/gen_iters.py
@@ -11,16 +11,66 @@ def gen():
 
 
 b = gen()
-print(b)
+# print(b)
+#
+# print(next(b))
+# print(next(b))
 
-print(next(b))
-print(next(b))
 
-
+# b_iter = iter(b)
 # while True:
 #     try:
-#         print(next(b))
+#         print(next(b_iter))
 #     except StopIteration:
 #         break
 
 
+class Iterator:
+
+    def __iter__(self):
+        ...
+
+    def __next__(self):
+        ...
+
+
+class Iter:
+
+    def __init__(self, data, callback):
+        # self._data = iter(data)
+        self._index = 0
+        self._max_value = 20
+        self.callback = callback
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # if self._index < len(self._data):
+        #     val = self._data[self._index]
+        #     self._index += 1
+        #
+        #     return val
+
+        # return self.callback(next(self._data))
+
+        if self._index < self._max_value:
+            val = self._index
+            self._index += 1
+            return val
+
+        raise StopIteration()
+
+    def reset(self):
+        self._index = 0
+
+
+if __name__ == "__main__":
+    iterator = Iter(big_data)
+
+    for item in iterator:
+        print(item)
+
+    iterator.reset()
+    for item in iterator:
+        print(item)
