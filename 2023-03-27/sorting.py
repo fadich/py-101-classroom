bcb3cdb058482ea15e54c05fa594b5abaa9bfe69
Fadi Ahmad <fadich95@gmail.com>
2023-03-27 20:32:44 +0300

    2023-03-27

--- 2023-03-27/sorting.py
+++ 2023-03-27/sorting.py
@@ -46,6 +46,32 @@ def count_sort(array: List[Any]):
     return result
 
 
+def merge_arrays(a, b):
+    result = []
+    index_a = 0
+    index_b = 0
+
+    while index_a < len(a) and index_b < len(b):
+        element_a = a[index_a]
+        element_b = b[index_b]
+        if element_a < element_b:
+            result.append(element_a)
+            index_a += 1
+        else:
+            result.append(element_b)
+            index_b += 1
+
+    while index_a < len(a):
+        result.append(a[index_a])
+        index_a += 1
+
+    while index_b < len(b):
+        result.append(b[index_b])
+        index_b += 1
+
+    return result
+
+
 if __name__ == "__main__":
     heroes = ["gandalf", "frodo", "spiderman", "aragorn", "arven"]
 
@@ -68,3 +94,5 @@ if __name__ == "__main__":
 
     print("COUNT")
     assert count_sort(heroes) == ["aragorn", "arven", "frodo", "gandalf", "spiderman"]
+
+    assert merge_arrays([4, 5, 8, 9, 10], [1, 2, 3, 5, 7, 12, 16, 20]) == [1, 2, 3, 4, 5, 5, 7, 8, 9, 10, 12, 16, 20]
