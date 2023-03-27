5d890892253bf0caa1e9668f785521555de3f208
Fadi Ahmad <fadich95@gmail.com>
2023-03-27 21:17:08 +0300

    2023-03-27

---
+++ 2023-03-27/threads.py
@@ -0,0 +1,10 @@
+import threading
+
+
+if __name__ == "__main__":
+    while True:
+        pass
+
+a = 1                   # d = 1
+b = 2                   # b = 15
+c = 1 + b / a           # c = 10 + 8 * d / b
